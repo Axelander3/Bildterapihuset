dfe21fca0e3c5d77ac6dd53fe537586f403c8037
Axelander3 <te15.alexander.mattsson@umea.nti.se>
2018-02-13 15:14:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bildterapihuset</title>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+
+</head>
+<body>
+	<div class="container">
+		<nav class="nav-bar">
+
+				<img id="logo" class="button" src="logga.svg">
+
+				<a class="button" href="">Startsida</a>
+
+				<a class="button" href="">Aktuellt</a>
+
+				<a class="button" href="">Om oss</a>
+
+				<a class="button" href="">Resurser</a>
+
+				<a class="button" href="">Kontakt</a>
+		</nav>
+	</div>
+
+</body>
+</html>
